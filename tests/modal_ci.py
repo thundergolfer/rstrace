c54df8e17efc08e25e7009a487567f8b77272995
Jonathon Belotti <jonathon@modal.com>
2025-08-09 23:12:50 +0000

    Add GPU test script

---
+++ tests/modal_ci.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+
+import modal
+
+image = modal.Image.debian_slim().pip_install("torch").apt_install(
+    "build-essential",
+    "curl",
+    "pkg-config",
+    "libssl-dev",
+    "git",
+    "clang",
+).run_commands(
+    "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y",
+)
+
+
+app = modal.App(
+    name="rstrace-gpu-ci-testing", 
+    image=image,
+    secrets=[modal.Secret.from_name("rstrace-github-token")],
+)
+
+@app.function(gpu="any", timeout=240)
+def test_on_gpu(commit_sha: str = ""):
+    token = os.environ["GITHUB_TOKEN"]
+    address = f"https://{token}@github.com/thundergolfer/rstrace.git"
+    if commit_sha:
+        address = f"{address}#commit={commit_sha}"
+
+    subprocess.run(f"git clone {address}", shell=True, check=True)
+    subprocess.run(f"cd rstrace && git checkout {commit_sha}", shell=True, check=True)
+    subprocess.run("/root/.cargo/bin/cargo install rstrace", shell=True, check=True)
+    subprocess.run("cd rstrace/tests && ./gpu.sh", shell=True, check=True)
+
+
+@app.local_entrypoint()
+def main(commit_sha: str = ""):
+    if commit_sha:
+        print(f"Testing commit {commit_sha}")
+    test_on_gpu.remote(commit_sha)
